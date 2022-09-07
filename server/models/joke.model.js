2cb6cbd70f46d48b2a506284c19aa35d3dcc19ec
brandontaylor156 <brandon.david4@outlook.com>
2022-09-07 12:14:01 -0700

    jokes api

---
+++ server/models/joke.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const JokeSchema = new mongoose.Schema({
+	setup: {type: String, required: true},
+	punchline: {type: String, required: true }
+}, 
+{
+	timestamps: true
+});
+
+const Joke = mongoose.model("Joke", JokeSchema);
+
+module.exports = Joke;
